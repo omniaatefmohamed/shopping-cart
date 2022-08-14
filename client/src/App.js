83a33ca5cf4a393614ebfd2561f0a1f256730719
omniaatefmohamed <omnia.atef.mohamed@gmail.com>
2022-08-14 14:22:47 +0200

    products comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
+import React , {useState} from 'react'
+
+import Products from './components/Products/Products'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import data from './data.json'
 import './App.css';
 
 function App() {
+  const [products,setProducts] = useState(data)
+  console.log(products)
   return (
     <div className="App">
       <Header />
-      <div class="content">
-        content
+      <div className='main-content'>
+          <Products products={products}/>
       </div>
      <Footer />
     </div>
