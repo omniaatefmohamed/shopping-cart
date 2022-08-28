0a3a8cf627c1e7b79e2e2384473f7c9885736e28
omniaatefmohamed <omnia.atef.mohamed@gmail.com>
2022-08-28 13:15:30 +0200

    cart style

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -4,24 +4,29 @@ import './../../css/Cart/Cart.css'
 
 function Cart(props) {
     return (
-        <div className='container-fluid'>
+        <div className='container'>
+            <p>There Is No Items</p>
+            <h2>There is {props.productsNumbers} Items In Cart</h2>
+            <hr/>
             {props.products.map(item => (
                 <div className='row'>
                     <div className='col-12'>
-                        <div className='cart-container d-flex justify-content-between align-items-center'>
-                            <div className='cart-item border'>
+                        <div className='cart-container'>
+                            <div className='cart-item border d-flex justify-content-between align-items-center'>
                                 <img src="images/image1.png" alt="image1" />
                                 <div className='data-cart'>
-                                    <p>{props.products.name}</p>
-                                    <p>{props.products.price}</p>
+                                    <p className="mb-0">Product Name</p>
+                                    <p className="mb-0">Qty : 1</p>
+                                    <p className="mb-0">Price : 1</p>
                                 </div>
+                                <button className='remove-btn'>Remove</button>
                             </div>
                         </div>
                     </div>
                 </div>
             ))
             }
-        </div >
+        </div>
     )
 }
 
